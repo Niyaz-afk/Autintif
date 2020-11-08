7b0601fc4a073da4fd78a28563340ca9c642b258
Niyaz <yakupovniyaz@gmail.com>
2020-11-08 13:48:10 +0300

    'test'

---
+++ userauth/views.py
@@ -0,0 +1,16 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+# Create your views here.
+
+class UserByToken(APIView):
+
+    def post(self,request, format=None):
+
+        data= {
+        "id" : str(request.user.id),
+        "username" : str(request.user.username),
+
+
+        }
+        return Response(data, status= status.HTTP_201_CREATED)
